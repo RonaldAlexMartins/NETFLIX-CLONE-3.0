ba58a45faeff3b49c3a313c77625c826a03efbc6
RonaldAlexMartins <ronny_nascimento@hotmail.com>
2022-12-09 19:52:24 -0300

    criacao do servidor estatico c/ express

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const path = require("path")
 
-let initialPath = Path.join(__dirname, "public")
+let initialPath = path.join(__dirname, "public")
 
 let app = express()
 app.use(express.static(initialPath))
 
-app.get("/")
+app.get("/", (req, res) =>{
+  res.sendFile(path.join(initialPath, "index.html"))
+})
+
+app.listen(3333, () =>{
+  console.log("Server start up!")
+})
